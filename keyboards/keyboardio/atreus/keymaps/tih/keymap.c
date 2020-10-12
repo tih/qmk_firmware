3a579fb176846285e94b7fc98968058cf344fedf
Tom Ivar Helbekkmo <tih@hamartun.priv.no>
2020-10-12 19:42:21 +0200

    toggle navi

--- keyboards/keyboardio/atreus/keymaps/tih/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/tih/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                              KC_Y,          KC_U,         KC_I,         KC_O,         KC_P,
     LCTL_T(KC_A), LSFT_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), LT(NUMS,KC_G),                     LT(NUMS,KC_H), LGUI_T(KC_J), LALT_T(KC_K), LSFT_T(KC_L), LCTL_T(KC_SCLN),
     KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,          SUP_LEFT, SUP_RGHT, KC_N,          KC_M,         KC_COMM,      KC_DOT,       KC_SLSH ,
-    KC_LGUI,      KC_LALT,      KC_GRV,       KC_EQL,       KC_BSPC,       KC_DEL,   KC_ENT,   KC_SPC,        KC_LBRC,      KC_RBRC,      KC_ALGR,      TT(NAVI)),
+    KC_LGUI,      KC_LALT,      KC_GRV,       KC_EQL,       KC_BSPC,       KC_DEL,   KC_ENT,   KC_SPC,        KC_LBRC,      KC_RBRC,      KC_ALGR,      TG(NAVI)),
 
   [NUMS] = LAYOUT(
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
